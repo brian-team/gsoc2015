518035c8373bbe63aedb0ba0b82a911d6aea86c7
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-05-18 13:54:42 +0200

    Remove `(unless refractory)` from the equations for now

--- IF.py
+++ IF.py
@@ -6,7 +6,7 @@ import sys
 duration = 1*second
 # tau = 10*ms
 eqs = '''
-dv/dt = (v0 - v) / tau : volt (unless refractory)
+dv/dt = (v0 - v) / tau : volt
 v0 : volt
 '''
 n = 1000
@@ -15,7 +15,7 @@ group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
                     refractory=5*ms, namespace=_namespace)
 group.v = 0*mV
 group.v0 = '20*mV * i / (n-1)'
-
+print group.equations._equations
 net = {}
 export_to_lems(net, str(sys.argv[0].split(".")[0]))
 
